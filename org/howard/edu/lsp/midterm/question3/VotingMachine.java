4b4991e79c1ec1a7e7e99a5bf13cfc06e93b88a8
ReihannaM <168609628+ReihannaM@users.noreply.github.com>
2025-03-20 20:04:03 -0400

    Create VotingMachine.java
    voting machine

---
+++ org/howard/edu/lsp/midterm/question3/VotingMachine.java
@@ -0,0 +1,38 @@
+package src.org.howard.edu.lsp.midterm.question3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class VotingMachine {
+    private Map<String, Integer> votes;
+
+    public VotingMachine() {
+        votes = new HashMap<>();
+    }
+
+    public void addCandidate(String name) {
+        if (!votes.containsKey(name)) {
+            votes.put(name, 0);
+        }
+    }
+
+    public boolean castVote(String name) {
+        if (votes.containsKey(name)) {
+            votes.put(name, votes.get(name) + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public String getWinner() {
+        String winner = null;
+        int maxVotes = 0;
+        for (Map.Entry<String, Integer> entry : votes.entrySet()) {
+            if (entry.getValue() > maxVotes) {
+                maxVotes = entry.getValue();
+                winner = entry.getKey();
+            }
+        }
+        return winner + " WINS with " + maxVotes + " votes!!";
+    }
+}
